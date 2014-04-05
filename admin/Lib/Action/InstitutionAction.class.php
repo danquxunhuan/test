ff7866a1a5736267d25064ebda4650b6f9a2e62c
danquxunhuan <sdszwq@126.com>
2014-04-05 10:49:24 +0800

    教育机构功能完成，后台的

--- admin/Lib/Action/InstitutionAction.class.php
+++ admin/Lib/Action/InstitutionAction.class.php
@@ -28,9 +28,57 @@ class InstitutionAction extends CommonAction{
 		}
 		echo json_encode($msg);
 	}
-
+    //show add html
 	public function add(){
 		$this->display();
+	}
+
+    //insert institution edu
+	public function add_edu(){
+		$date=array(
+			'name'=>trim($_POST['name']),
+			'tel'=>trim($_POST['tel']),
+			'address'=>trim($_POST['add']),
+			'info'=>trim($_POST['info']),
+			'linkman'=>trim($_POST['linkman'])
+			);
+		$Institution=M("Institution_edu");
+		$info=$Institution->add($date);
+		if($info){
+			$msg=1;
+		}else{
+			$msg=0;
+		}
+		echo json_encode($msg);
+	}
+
+	// show modify institution edu info html
+	public function modify(){
+		$id=intval($_GET['id']);
+		$Institution=M("Institution_edu");
+		$info=$Institution->where("id=".$id)->find();
+		$this->assign("info",$info);
+		$this->display();
+	}
+
+	//modify institution edu on update
+	public function modify_info(){
+		$date=array(
+			'id'=>intval($_POST['id']),
+			'name'=>trim($_POST['name']),
+			'tel'=>trim($_POST['tel']),
+			'address'=>trim($_POST['add']),
+			'info'=>trim($_POST['info']),
+			'linkman'=>trim($_POST['linkman'])
+			);
+		$Institution=M("Institution_edu");
+		$info=$Institution->save($date);
+		if($info){
+			$msg=1;
+		}else{
+			$msg=0;
+		}
+		echo json_encode($msg);
 	}
 }
 
--- admin/Runtime/Cache/7443eb3a74289dcc56a81406b4220d46.php
+++ admin/Runtime/Cache/7443eb3a74289dcc56a81406b4220d46.php
@@ -12,11 +12,11 @@
 <link rel="stylesheet" type="text/css" href="__PUBLIC__/js/calendar/calendar-blue.css"/>
 <link rel="stylesheet" href="__PUBLIC__/css/admin.css"/>
 <script src="__PUBLIC__/js/jquery-1.9.0.min.js"></script>
-<!-- <script src="__PUBLIC__/js/jquery.lazyload.js"></script> -->
+<!--script src="__PUBLIC__/js/jquery.lazyload.js"></script-->
 <script src="__PUBLIC__/js/functions.js"></script>
 <script src="__PUBLIC__/js/jquery.form.js"></script>
 <script src="__PUBLIC__/js/asyncbox/asyncbox.js"></script>
-<!-- <script src="__PUBLIC__/js/calendar/calendar.js"></script> -->
+<script src="__PUBLIC__/js/calendar/calendar.js"></script>
 
 <!--***************************top******************************-->
 	<div class="top">
@@ -52,6 +52,7 @@
         <li>&nbsp;&nbsp;&nbsp;<a href="__APP__/Yueke" >约课列表</a></li>
         <li>&nbsp;&nbsp;&nbsp;<a href="__APP__/Fangan" >设计方案列表</a></li>
         <li>&nbsp;&nbsp;&nbsp;<a href="__APP__/Lesson" >课程列表</a></li>
+        <li>&nbsp;&nbsp;&nbsp;<a href="__APP__/Institution" >教育机构</a></li>
      </ul>
 </div>
                 <div id="Right"  class="right">
